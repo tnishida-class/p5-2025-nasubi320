1373f9c4d495bc474f93151459d921596411d3d2
nasubi320 <2400589h@gsuite.kobe-u.ac.jp>
2025-11-24 11:56:01 +0900

    fw

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -5,12 +5,19 @@ let fontSize = 30;
 let text2 = "GraduateSchoolofInterculturalStudies,KobeUniversity";
 let letters2 = [];
 let fontSize2 = 10;
+
+
+
+//四角
 let Rwidth = 0;
-let angle =0;
+
+let flowers = [];
+let flowerCount = 8;
+
 
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);　//ウィンドウのサイズを変えても対応可
+  resizeCanvas(windowWidth, windowHeight);//ウィンドウのサイズを変えても対応可
 }
 
 function setup() {
@@ -29,7 +36,7 @@ function setup() {
       delay: i * 10, // 何フレーム後に動き出すか
       speed: 3,
       arrived: false
-    }; letters.push(c);　//　文字に情報を追加、その文字を配列に追加
+    }; letters.push(c);//　文字に情報を追加、その文字を配列に追加
 
   }
 
@@ -48,26 +55,32 @@ function setup() {
     }; letters2.push(c2);　//　文字に情報を追加、その文字を配列に追加
 
   }
+ 
+  for (let a = 0; a < flowerCount; a++) {
+    let f = {
+      x: random(50, windowWidth - 50),
+      y: random(50, windowHeight - 50),
+      r: random(10, 30),
+      angle:0
 
+    }; flowers.push(f);
+
+
+  }
 }
 
-function flower(cx, cy, r,angle) {
 
-  push();               // 座標系を保存
-    // 花の中心を原点に移動
-  rotate(angle);
+
+function ajisai(cx, cy, r) {
+
   
-  for(let i = 0; i < 4; i++){
-    const theta = TWO_PI * i /4 
-    const x = cx + cos(theta) * r;
-    const y = cy + sin(theta) * r;
 
   ellipse(cx + r, cy, r * 2);
   ellipse(cx, cy - r, r * 2);
-  ellipse(cx - r, cy, r *2 );
+  ellipse(cx - r, cy, r * 2);
   ellipse(cx, cy + r, r * 2);
-}
- pop(); 
+
+  
 
 
 
@@ -79,33 +92,25 @@ function flower(cx, cy, r,angle) {
 
 function draw() {
   background(255);
-　
-  noStroke();
-  angle += 0.05;
-  fill(0, 0, 255);
-  flower(80, windowHeight / 2 - fontSize - 300, 30);
-  fill(0, 0, 139);
-  flower(150, windowHeight / 2 - fontSize - 200, 10);
-  fill(148, 0, 211);
-  flower(windowWidth - 100, windowHeight / 2 + fontSize + 300, 20);
-  fill(138, 43, 226);
-  flower(windowWidth - 50, windowHeight / 2 + fontSize + 200, 15);
-  fill(139, 0, 139);
-  flower(windowWidth - 200, windowHeight / 2 + fontSize + 150, 30);
-   
 
+  for (let i = 0; i < flowers.length; i++) {
+  let f = flowers[i];
+  fill(150,50,200,200); 
+  noStroke();
+  ajisai(f.x,f.y,f.r);
+}
 
 
   noStroke();
   fill(220, 230, 255);
   rect(0, windowHeight / 2 - fontSize * 2, Rwidth, fontSize * 4)
-  Rwidth += 2;
+  Rwidth += 5;
 
   // 日本語
   for (let i = 0; i < letters.length; i++) {
 
     let l = letters[i];
-    // 時間が来たら落下開始
+
     if (frameCount > l.delay) {　//ある時間(フレーム、delay)を過ぎたら
       if (!l.arrived) {
         l.y += l.speed;
@@ -134,7 +139,7 @@ function draw() {
     if (frameCount > l2.delay) {　//ある時間(フレーム、delay)を過ぎたら
       fill(0);
     }
-    else { fill(255); }
+    else { fill(0,0,0,0); }
     textSize(fontSize2);
     textAlign(CENTER, CENTER);
     textStyle(NORMAL);
@@ -148,64 +153,4 @@ function draw() {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
